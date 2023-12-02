a9619a305bbb4664a3bf8c7bb1efe0b65757d308
idomshi <56523771+idomshi@users.noreply.github.com>
2023-12-02 20:21:03 +0900

    チュートリアルに従ってログイン機能を作る

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,8 +4,12 @@
 
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/prepaid.tsx";
-import * as $3 from "./routes/score.tsx";
+import * as $2 from "./routes/api/login.ts";
+import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/logout.ts";
+import * as $5 from "./routes/prepaid.tsx";
+import * as $6 from "./routes/score.tsx";
+import * as $7 from "./routes/secret_or_redirect.tsx";
 import * as $$0 from "./islands/TheForm.tsx";
 import * as $$1 from "./islands/assets/store.ts";
 
@@ -13,8 +17,12 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
     "./routes/_app.tsx": $1,
-    "./routes/index.tsx": $2,
-    "./routes/score.tsx": $3,
+    "./routes/api/login.ts": $2,
+    "./routes/index.tsx": $3,
+    "./routes/logout.ts": $4,
+    "./routes/prepaid.tsx": $5,
+    "./routes/score.tsx": $6,
+    "./routes/secret_or_redirect.tsx": $7,
   },
   islands: {
     "./islands/TheForm.tsx": $$0,
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,46 @@
-import { PageProps } from "$fresh/server.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { getCookies } from "$std/http/cookie.ts";
 
-export default function Index(props: PageProps) {
-  return <div>Hello</div>;
+interface Data {
+  isAllowed: boolean;
+}
+
+export const handler: Handlers = {
+  GET(req, ctx) {
+    const cookies = getCookies(req.headers);
+    return ctx.render!({ isAllowed: cookies.auth === "bar" });
+  },
+};
+
+export default function Home({ data }: PageProps<Data>) {
+  return (
+    <div class="p-2">
+      <div>
+        <p>
+          <a href="secret_or_redirect" class="underline">
+            for authenticated users only
+          </a>
+        </p>
+        <p>
+          You currently {data.isAllowed ? "are" : "are not"} logged in.
+        </p>
+      </div>
+      {!data.isAllowed ? <Login /> : <a href="/logout">Logout</a>}
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <form action="/api/login" method="post" class="flex flex-row gap-2">
+      <input type="text" name="username" class="border rounded p-1" />
+      <input type="password" name="password" class="border rounded p-1" />
+      <button
+        type="submit"
+        class="rounded px-4 py-1 border bg-gray-400 text-white"
+      >
+        Submit
+      </button>
+    </form>
+  );
 }
